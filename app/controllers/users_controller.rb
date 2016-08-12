ee839a84567f3cfd591cd86a41a3138b1057d208
Michael Levin <michaellevin.dev@gmail.com>
2016-08-12 20:25:28 +0000

    New User adding and showing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,19 @@
      end 
      
      def create
-      #render plain: params[:user ].inspect
       @user = User.new(user_params)
-      @user.save
-      redirect_to users_show(@user)
+      if @user.save
+       flash[:notice] = "New user has been created."
+       redirect_to user_path(@user)
+      else
+       #flash[:notice] = "User HAS NOT been created."
+       render :new 
+      end
      end
+     
+    def show
+     @user = User.find(params[:id]) 
+    end
      
      private
       def user_params
